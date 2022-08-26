b36bda6bfa0de7025b8fee6912b8a38b9b9e7b96
Mohammed Alhawamdeh <57315004+MohammedAlhawamdeh@users.noreply.github.com>
2022-08-26 22:48:06 +0100

    Adding Icon Package

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 
 import React from 'react'
+import { TbSquarePlus } from 'react-icons/tb';
 
 const App = () => {
   return (
-    <div>Hello everyone, what do you think all stuff I want? thanks cheers mate you welcome mate.</div>
+    <div>Hello everyone, <TbSquarePlus/> what do you think all stuff I want? thanks cheers mate you welcome mate.</div>
   )
 }
 
