adf695e3c47bb4804b422c4425d2ecbe3c2361af
Mohammed Alhawamdeh <57315004+MohammedAlhawamdeh@users.noreply.github.com>
2022-08-28 20:37:21 +0100

    Adding Todos with completion & deletion

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
 import './App.css';
-
-import React from 'react'
-import { TbSquarePlus } from 'react-icons/tb';
+import React , {useState} from 'react'
+import { GoPlus } from 'react-icons/go'
+import { ImCheckboxChecked } from "react-icons/im";
+import { CgTrash } from "react-icons/cg";
 
 const App = () => {
+  const [todos , setTodos] = useState([])
+  const [input , setInput] = useState('')
+  const handleChange = (e) =>{
+    setInput(e.target.value)
+  }
+  const handleClick = (e) =>{
+    e.preventDefault()
+    setTodos([...todos , {input , completed : false}])
+  }
+  const handleDelete = (e,pickedTodo) =>{
+    e.preventDefault()
+    let filteredTodos = todos.filter(t=>(pickedTodo !== t.input))
+    setTodos(filteredTodos)
+  }
+  const handleCompleted = (e,pickedTodo) =>{
+    e.preventDefault()
+    pickedTodo.completed = !pickedTodo.completed
+    setTodos([ ...todos])
+  }
+  
   return (
-    <div>Hello everyone, <TbSquarePlus/> what do you think all stuff I want? thanks cheers mate you welcome mate.</div>
+    <>
+    <h1>To Do List</h1>
+    <input type="text" value={input} onChange={handleChange} />
+    <button onClick={handleClick}><GoPlus/></button>
+    {
+      todos.map(todo =>(
+        <div>
+        <span className={(todo.completed)?'btn-line':'btn'}>{todo.input}</span>
+        <button onClick={(e)=>handleCompleted(e,todo)}><ImCheckboxChecked/></button>
+        <button onClick={(e)=>handleDelete(e,todo.input)}><CgTrash/></button>
+        </div>
+      ))
+    }
+    </>
   )
 }
 
